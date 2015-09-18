8b1bc88fcd7e8d565402679f8b8111f488100866
Luis Pabón <lpabon@redhat.com>
2015-09-18 10:35:38 -0400

    Update large functional tests
    Allows faster creation of VMs by creating the VMs in parallel. Also
    creates 5 clusters of 6 nodes each.

--- tests/functional/large/vagrant/up.sh
+++ tests/functional/large/vagrant/up.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-vagrant up --provider=libvirt 
+# Based on blog:
+# https://coderwall.com/p/gy2eng/bring-vagrant-vms-up-in-parallel
+vmup() {
+
+    # Start all vms
+    for id in {0..29}; do
+    vm="storage${id}"
+    echo "[$vm] Bringing up VM"
+
+    vagrant up $vm --provider=libvirt --no-provision &
+    done
+
+    # Start client
+    vagrant up client --provider=libvirt --no-provision &
+
+    # Make sure all child processes have finished before exiting.
+    wait
+}
+
+vmup
 vagrant provision
 vagrant halt
-vagrant up --provider=libvirt
-vagrant provision
+vmup
