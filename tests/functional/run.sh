92cb13bb9cb8a3e5131e0f941d0abc89de023431
Luis Pabón <lpabon@redhat.com>
2016-06-17 15:26:17 -0400

    CI Functional Tests
    Functional tests are based on a shell script in
    tests/functional which execute all tests under
    that directory.  Each of these tests setup their
    own gluster cluster using Vagrant and Ansible, then
    run a test program written in Go.
    
    Signed-off-by: Luis Pabón <lpabon@redhat.com>

---
+++ tests/functional/run.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+fail() {
+    echo $1
+    exit 1
+}
+
+println() {
+    echo "==> $1"
+}
+
+starttime=`date`
+export PATH=$PATH:.
+
+# Check go can build
+if [ -z $GOPATH ] ; then 
+    fail "GOPATH must be specified"
+fi
+
+# Clean up
+rm -f heketi-server > /dev/null 2>&1
+
+# Check each dir for tests
+results=0
+for testDir in * ; do
+    if [ -x $testDir/run.sh ] ; then
+        println "TEST $testDir"
+        cd $testDir 
+          run.sh
+        result=$?
+        cd ..
+
+        if [ $result -ne 0 ] ; then
+            println "FAILED $testDir"
+            results=1
+        else
+            println "PASSED $testDir"
+        fi
+    fi
+done
+
+# Summary
+println "Started $starttime"
+println "Ended `date`"
+if [ $results -eq 0 ] ; then
+    println "PASSED"
+else
+    println "FAILED"
+fi
+
+exit $results
