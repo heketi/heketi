71ec0a1a807b8c957bbc364ed4fb283a02e6d7df
Mohamed Ashiq Liyazudeen <mliyazud@redhat.com>
2016-07-12 12:34:50 +0530

    heketi-cli: heketi-cli command to return help
    Signed-off-by: Mohamed Ashiq Liyazudeen <mliyazud@redhat.com>

--- client/cli/go/cmds/root.go
+++ client/cli/go/cmds/root.go
@@ -47,6 +47,8 @@ var RootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		if version {
 			fmt.Printf("heketi-cli %v\n", HEKETI_CLI_VERSION)
+		} else {
+			cmd.Usage()
 		}
 	},
 }
@@ -73,7 +75,7 @@ func initConfig() {
 	// Check server
 	if options.Url == "" {
 		options.Url = os.Getenv("HEKETI_CLI_SERVER")
-		if options.Url == "" && !version {
+		if options.Url == "" && !version && !(len(RootCmd.Flags().Args()) <= 0) {
 			fmt.Fprintf(stderr, "Server must be provided\n")
 			os.Exit(3)
 		}
