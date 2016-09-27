0cdbcd6d81282276ac37a5eb37e5f5ca8b404dd2
Mohamed Ashiq Liyazudeen <mliyazud@redhat.com>
2016-09-27 19:31:18 +0530

    Kubernetes Deployment:
    The artifacts for glusterfs and heketi deployment in kubernetes.
    Bearer Token authentication.
    User can now create a service account and use it for heketi to validate the kubernetes connection.
    
    Closes: #508, #521, & #507
    
    Signed-off-by: Mohamed Ashiq Liyazudeen <mliyazud@redhat.com>

--- executors/kubeexec/config.go
+++ executors/kubeexec/config.go
@@ -28,6 +28,9 @@ type KubeConfig struct {
 	User      string `json:"user"`
 	Password  string `json:"password"`
 	Namespace string `json:"namespace"`
+	// Use Secrets to get the Bearerkey
+	UseSecrets bool   `json:"use_secrets"`
+	TokenFile  string `json:"token"`
 
 	// Use POD name instead of using label
 	// to access POD
