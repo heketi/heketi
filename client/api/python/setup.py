029b0ef0358a7c289bca8a4a331efc22405c72e3
Luis Pabón <lpabon@redhat.com>
2016-11-02 23:21:17 -0400

    Change license to GPL2/LGPL3
    Signed-off-by: Michael Adam <obnox@redhat.com>
    Signed-off-by: Luis Pabón <lpabon@redhat.com>

--- client/api/python/setup.py
+++ client/api/python/setup.py
@@ -1,17 +1,10 @@
 # Copyright (c) 2016 heketi authors
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This file is licensed to you under your choice of the GNU Lesser
+# General Public License, version 3 or any later version (LGPLv3 or
+# later), or the GNU General Public License, version 2 (GPLv2), in all
+# cases as published by the Free Software Foundation.
+
 
 from setuptools import setup, find_packages
 
@@ -19,7 +12,7 @@ setup(
     name='heketi',
     version='3.0.0',
     description='Python client library for Heketi',
-    license='Apache License (2.0)',
+    license='LGPLv3+ or GPLv2'
     author='Luis Pabon',
     author_email='lpabon@redhat.com',
     url='https://github.com/heketi/heketi/tree/master/client/api/python',
@@ -30,7 +23,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
