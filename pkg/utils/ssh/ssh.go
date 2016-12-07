c46362966184f422ec40c1d2f164b5a8906fb535
Raghavendra Talur <rtalur@redhat.com>
2016-12-07 18:39:41 +0530

    tests: add a test for checking if volume is group writable
    Signed-off-by: Raghavendra Talur <rtalur@redhat.com>

--- pkg/utils/ssh/ssh.go
+++ pkg/utils/ssh/ssh.go
@@ -98,6 +98,21 @@ func NewSshExecWithKeyFile(logger *utils.Logger, user string, file string) *SshE
 	return sshexec
 }
 
+// This function requires the password string to be crypt encrypted
+func NewSshExecWithPassword(logger *utils.Logger, user string, password string) *SshExec {
+
+	sshexec := &SshExec{}
+	sshexec.logger = logger
+
+	// Define the Client Config as :
+	sshexec.clientConfig = &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{ssh.Password(password)},
+	}
+
+	return sshexec
+}
+
 // This function was based from https://github.com/coreos/etcd-manager/blob/master/main.go
 func (s *SshExec) ConnectAndExec(host string, commands []string, timeoutMinutes int, useSudo bool) ([]string, error) {
 
