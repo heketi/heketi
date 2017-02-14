429e3cf1e0f7bbfe4ddb796ddf3cacd4ebeb87a4
Luis Pabón <luis.pabon@coreos.com>
2017-02-14 15:24:18 -0500

    Update Dockerfiles for Release 4

---
+++ extras/docker/fromsource/heketi-start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -f /backupdb/heketi.db ] ; then
+    cp /backupdb/heketi.db /var/lib/heketi/heketi.db
+    if [ $? -ne 0 ] ; then
+        echo "Unable to copy database"
+        exit 1
+    fi
+    echo "Copied backup db to /var/lib/heketi/heketi.db"
+fi
+
+/usr/bin/heketi -config=/etc/heketi/heketi.json
