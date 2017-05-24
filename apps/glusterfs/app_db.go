6c3db2c58c83958e30417c26994a65fe4f5eb9d3
Raghavendra Talur <rtalur@redhat.com>
2017-05-24 17:01:10 +0530

    create a complete json
    Signed-off-by: Raghavendra Talur <rtalur@redhat.com>

--- apps/glusterfs/app_db.go
+++ apps/glusterfs/app_db.go
@@ -16,9 +16,22 @@ import (
 	"github.com/boltdb/bolt"
 )
 
+type DbDump struct {
+	Clusters []ClusterEntry `json:"clusterentries"`
+	Volumes  []VolumeEntry  `json:"volumeentries"`
+	Bricks   []BrickEntry   `json:"brickentries"`
+	Nodes    []NodeEntry    `json:"nodeentries"`
+	Devices  []DeviceEntry  `json:"deviceentries"`
+}
+
 // DbDump ... Creates a JSON output representing the state of DB
 func (a *App) DbDump(w http.ResponseWriter, r *http.Request) {
+	var dump DbDump
+	clusterEntryList := make([]ClusterEntry, 0)
 	volEntryList := make([]VolumeEntry, 0)
+	brickEntryList := make([]BrickEntry, 0)
+	nodeEntryList := make([]NodeEntry, 0)
+	deviceEntryList := make([]DeviceEntry, 0)
 
 	err := a.db.View(func(tx *bolt.Tx) error {
 
@@ -43,10 +56,16 @@ func (a *App) DbDump(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	dump.Clusters = clusterEntryList
+	dump.Volumes = volEntryList
+	dump.Bricks = brickEntryList
+	dump.Nodes = nodeEntryList
+	dump.Devices = deviceEntryList
+
 	// Write msg
-	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(volEntryList); err != nil {
+	if err := json.NewEncoder(w).Encode(dump); err != nil {
 		panic(err)
 	}
 }
