6ec9ce7537dc35e6aad926c9eb14e306a8fe33d0
Mohamed Ashiq Liyazudeen <mliyazud@redhat.com>
2017-07-03 19:52:06 +0530

    Unit test initial
    Signed-off-by: Mohamed Ashiq Liyazudeen <mliyazud@redhat.com>

---
+++ apps/glusterfs/block_volume_entry_test.go
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2015 The heketi Authors
+//
+// This file is licensed to you under your choice of the GNU Lesser
+// General Public License, version 3 or any later version (LGPLv3 or
+// later), or the GNU General Public License, version 2 (GPLv2), in all
+// cases as published by the Free Software Foundation.
+//
+package glusterfs
+
+import "github.com/heketi/heketi/pkg/glusterfs/api"
+
+func createSampleBlockVolumeEntry(size int) *BlockVolumeEntry {
+	req := &api.BlockVolumeCreateRequest{}
+	req.Size = size
+
+	v := NewBlockVolumeEntryFromRequest(req)
+
+	return v
+}
