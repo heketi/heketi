4601a6560ff9a98b2950a40fbdea573f3162b16c
Dimitris Stafylarakis <xanias@gmail.com>
2017-08-29 14:28:56 +0200

    add copyright statement to new files

--- apps/glusterfs/georep.go
+++ apps/glusterfs/georep.go
@@ -49,6 +49,15 @@ func (v *VolumeEntry) GeoReplicationAction(db *bolt.DB,
 	return nil
 }
 
+//
+// Copyright (c) 2015 The heketi Authors
+//
+// This file is licensed to you under your choice of the GNU Lesser
+// General Public License, version 3 or any later version (LGPLv3 or
+// later), or the GNU General Public License, version 2 (GPLv2), in all
+// cases as published by the Free Software Foundation.
+//
+
 // NewGeoReplicationStatusResponse returns a geo-replication status response
 // populated with data from the executor
 func (v *VolumeEntry) NewGeoReplicationStatusResponse(executor executors.Executor,
