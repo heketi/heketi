c6728a0db111dee55ae7eb43699eb1b817eedfdc
Raghavendra Talur <rtalur@redhat.com>
2017-09-11 15:37:43 +0530

    adding missing --json flag to block delete cmd
    Signed-off-by: Raghavendra Talur <rtalur@redhat.com>

--- executors/sshexec/block_volume.go
+++ executors/sshexec/block_volume.go
@@ -88,7 +88,7 @@ func (s *SshExecutor) BlockVolumeDestroy(host string, blockHostingVolumeName str
 	godbc.Require(blockVolumeName != "")
 
 	commands := []string{
-		fmt.Sprintf("gluster-block delete %v/%v", blockHostingVolumeName, blockVolumeName),
+		fmt.Sprintf("gluster-block delete %v/%v --json", blockHostingVolumeName, blockVolumeName),
 	}
 
 	type CliOutput struct {
