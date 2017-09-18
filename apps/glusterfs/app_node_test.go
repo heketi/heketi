6c36cbefa382895418ff02ce62e9880c6c1c85f5
Michael Adam <obnox@redhat.com>
2017-09-18 10:35:56 +0200

    SQ: fix unit tests (missing argument for ClusterCreate)
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- apps/glusterfs/app_node_test.go
+++ apps/glusterfs/app_node_test.go
@@ -1377,7 +1377,11 @@ func TestNodeInfoAfterDelete(t *testing.T) {
 	tests.Assert(t, c != nil)
 
 	// Create Cluster
-	cluster, err := c.ClusterCreate()
+	cluster_req := &api.ClusterCreateRequest{
+		Block: true,
+		File:  true,
+	}
+	cluster, err := c.ClusterCreate(cluster_req)
 	tests.Assert(t, err == nil)
 
 	// Create Node
