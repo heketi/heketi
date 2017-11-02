75375127e3218868cfb409e905ec6eee8d571628
John Mulligan <jmulligan@redhat.com>
2017-11-02 16:05:23 -0400

    tests: support go tests exiting on first failing module
    Run go tests in a loop and use env var from parent script to
    support stopping on first failure. This also sets us up for
    generating coverage reports because it can only do coverage
    for one module at a time (coverage is not implemnted yet tho).
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/100-gotest.sh
+++ tests/100-gotest.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
 GOFILES="$(go list ./... | grep -v vendor)"
-exec go test ${GOFILES}
+# no special options, exec to go test w/ all pkgs
+if [[ ${HEKETI_TEST_EXITFIRST} != "yes" ]]; then
+	exec go test ${GOFILES}
+fi
+
+# our options are set so we need to handle each go package one
+# at at time
+failed=0
+for gofile in ${GOFILES}; do
+	go test "${gofile}"
+	[ $? -ne 0 ] && ((failed+=1))
+	if [[ ${failed} -ne 0 && ${HEKETI_TEST_EXITFIRST} = "yes" ]]; then
+		exit ${failed}
+	fi
+done
+exit ${failed}
