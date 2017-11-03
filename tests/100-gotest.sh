a2390a317c479e0d74d0fe83cc6d8f4527887915
John Mulligan <jmulligan@redhat.com>
2017-11-03 10:47:28 -0400

    tests: change gotest script to more properly refer to packages
    The original makefile version called the variable GOFILES however
    as the output of go list and input to go test are packages it
    makes sense to rename to GOPACKAGES, etc.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/100-gotest.sh
+++ tests/100-gotest.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-GOFILES="$(go list ./... | grep -v vendor)"
+GOPACKAGES="$(go list ./... | grep -v vendor)"
 # no special options, exec to go test w/ all pkgs
 if [[ ${HEKETI_TEST_EXITFIRST} != "yes" ]]; then
-	exec go test ${GOFILES}
+	exec go test ${GOPACKAGES}
 fi
 
 # our options are set so we need to handle each go package one
 # at at time
 failed=0
-for gofile in ${GOFILES}; do
-	go test "${gofile}"
+for gopackage in ${GOPACKAGES}; do
+	go test "$gopackage"
 	[ $? -ne 0 ] && ((failed+=1))
 	if [[ ${failed} -ne 0 && ${HEKETI_TEST_EXITFIRST} = "yes" ]]; then
 		exit ${failed}
