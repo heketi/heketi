0685a130995c2b6054deab414c559b16448c6a5e
Michael Adam <obnox@redhat.com>
2017-12-15 06:45:03 +0100

    app: use heketi/heketi/pkg/utils instead of heketi/utils
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- apps/glusterfs/app_block_volume_test.go
+++ apps/glusterfs/app_block_volume_test.go
@@ -23,8 +23,8 @@ import (
 	"github.com/boltdb/bolt"
 	"github.com/gorilla/mux"
 	"github.com/heketi/heketi/pkg/glusterfs/api"
+	"github.com/heketi/heketi/pkg/utils"
 	"github.com/heketi/tests"
-	"github.com/heketi/utils"
 )
 
 func TestBlockVolumeCreateBadJson(t *testing.T) {
