05bf4141dd7a9853c807615a6e5e5f4768ece3b1
Michael Adam <obnox@redhat.com>
2017-12-15 10:19:28 +0100

    tests:functional: enable shellcheck on lib.sh
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- tests/functional/lib.sh
+++ tests/functional/lib.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# FIXME HEKETI-SKIP-SHELLCHECK
 
 fail() {
-    echo "==> ERROR: $@"
+    echo "==> ERROR: $*"
     exit 1
 }
 
@@ -12,15 +11,15 @@ println() {
 
 _sudo() {
     if [ ${UID} = 0 ] ; then
-        ${@}
+        "${@}"
     else
-        sudo -E ${@}
+        sudo -E "${@}"
     fi
 }
 
 HEKETI_PID=
 start_heketi() {
-    ( cd $HEKETI_SERVER_BUILD_DIR && make && cp heketi $HEKETI_SERVER )
+    ( cd "$HEKETI_SERVER_BUILD_DIR" && make && cp heketi "$HEKETI_SERVER" )
     if [ $? -ne 0 ] ; then
         fail "Unable to build Heketi"
     fi
@@ -33,19 +32,19 @@ start_heketi() {
 }
 
 start_vagrant() {
-    cd vagrant
+    cd vagrant || fail "Unable to 'cd vagrant'."
     _sudo ./up.sh || fail "unable to start vagrant virtual machines"
     cd ..
 }
 
 teardown_vagrant() {
-    cd vagrant
+    cd vagrant || fail "Unable to 'cd vagrant'."
     _sudo vagrant destroy -f
     cd ..
 }
 
 run_tests() {
-    cd tests
+    cd tests || fail "Unable to 'cd tests'."
     go test -timeout=1h -tags functional
     gotest_result=$?
     cd ..
@@ -53,7 +52,7 @@ run_tests() {
 
 force_cleanup_libvirt_disks() {
     # Sometimes disks are not deleted
-    for i in `_sudo virsh vol-list default | grep "*.disk" | awk '{print $1}'` ; do
+    for i in $(_sudo virsh vol-list default | grep '\.disk' | awk '{print $1}') ; do
         _sudo virsh vol-delete --pool default "${i}" || fail "Unable to delete disk $i"
     done
 }
