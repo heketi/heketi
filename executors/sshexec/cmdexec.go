1ca69d34cd9dfb58a25fa0cdeb80736365c4b085
John Mulligan <jmulligan@redhat.com>
2018-01-20 13:50:44 -0500

    executors: start a common command based executor
    Begin to refactor the relationship between the "real" executor types.
    Create a cmdexec.go file that will contain the parts common to all
    command based excecutors (executors that run gluster, etc. command line
    tools).
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ executors/sshexec/cmdexec.go
@@ -0,0 +1,75 @@
+//
+// Copyright (c) 2018 The heketi Authors
+//
+// This file is licensed to you under your choice of the GNU Lesser
+// General Public License, version 3 or any later version (LGPLv3 or
+// later), or the GNU General Public License, version 2 (GPLv2), in all
+// cases as published by the Free Software Foundation.
+//
+
+package sshexec
+
+import (
+	"sync"
+
+	"github.com/heketi/heketi/pkg/utils"
+)
+
+//var (
+//	logger = utils.NewLogger("[sshexec]", utils.LEVEL_DEBUG)
+//)
+
+//type RemoteCommandTransport interface {
+//	RemoteCommandExecute(host string, commands []string, timeoutMinutes int) ([]string, error)
+//	RebalanceOnExpansion() bool
+//	SnapShotLimit() int
+//}
+
+type CmdExecutor struct {
+	Throttlemap map[string]chan bool
+	Lock        sync.Mutex
+
+	RemoteExecutor RemoteCommandTransport
+	Fstab          string
+}
+
+func (s *CmdExecutor) AccessConnection(host string) {
+	var (
+		c  chan bool
+		ok bool
+	)
+
+	s.Lock.Lock()
+	if c, ok = s.Throttlemap[host]; !ok {
+		c = make(chan bool, 1)
+		s.Throttlemap[host] = c
+	}
+	s.Lock.Unlock()
+
+	c <- true
+}
+
+func (s *CmdExecutor) FreeConnection(host string) {
+	s.Lock.Lock()
+	c := s.Throttlemap[host]
+	s.Lock.Unlock()
+
+	<-c
+}
+
+func (s *CmdExecutor) SetLogLevel(level string) {
+	switch level {
+	case "none":
+		logger.SetLevel(utils.LEVEL_NOLOG)
+	case "critical":
+		logger.SetLevel(utils.LEVEL_CRITICAL)
+	case "error":
+		logger.SetLevel(utils.LEVEL_ERROR)
+	case "warning":
+		logger.SetLevel(utils.LEVEL_WARNING)
+	case "info":
+		logger.SetLevel(utils.LEVEL_INFO)
+	case "debug":
+		logger.SetLevel(utils.LEVEL_DEBUG)
+	}
+}
