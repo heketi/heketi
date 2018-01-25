ffb9fea98bb7fa94917bccdb5bc2bc84e078ab89
John Mulligan <jmulligan@redhat.com>
2018-01-25 17:20:07 -0500

    tests: add test selection options to functional test script
    The top-level functional test script is useful for environment setup
    and pre-configuration. However it always ran all of the functional
    test suites. Give it a basic command line parser and options to
    select which suite to run.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/functional/run.sh
+++ tests/functional/run.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 
-TESTS="TestSmokeTest"
-TESTS="TestVolumeNotDeletedWhenNodeIsDown $TESTS"
-TESTS="TestVolumeSnapshotBehavior $TESTS"
-TESTS="TestManyBricksVolume $TESTS"
+show_help() {
+	echo "$0 [options]"
+	echo "  Options:"
+	echo "    --test T  Run test suite T (can be specified multiple times)"
+	echo "    --help    Display this help text"
+	echo ""
+}
+
+TESTS=()
+
+CLI="$(getopt -o h --long test:,help -n "$0" -- "$@")"
+eval set -- "${CLI}"
+while true ; do
+	case "$1" in
+		--test)
+			TESTS+=("$2")
+			shift
+			shift
+		;;
+		-h|--help)
+			show_help
+			exit 0
+		;;
+		--)
+			shift
+			break
+		;;
+		*)
+			echo "unknown option" >&2
+			exit 2
+		;;
+	esac
+done
+
+
+if [[ "${#TESTS[@]}" -eq 0 ]]; then
+	TESTS+=("TestSmokeTest")
+	TESTS+=("TestVolumeNotDeletedWhenNodeIsDown")
+	TESTS+=("TestVolumeSnapshotBehavior")
+	TESTS+=("TestManyBricksVolume")
+fi
 
 # install glide
 if ! command -v glide ; then
@@ -23,7 +60,7 @@ source ./lib.sh
 
 teardown_all() {
     results=0
-    for testDir in $TESTS ; do
+    for testDir in "${TESTS[@]}" ; do
         if [ -x "$testDir/teardown.sh" ] ; then
             println "TEARDOWN $testDir"
             cd "$testDir" || fail "Unable to 'cd $testDir'."
@@ -52,7 +89,7 @@ teardown_all
 
 # Check each dir for tests
 results=0
-for testDir in $TESTS ; do
+for testDir in "${TESTS[@]}" ; do
     if [ -x "$testDir/run.sh" ] ; then
         println "TEST $testDir"
         cd "$testDir" || fail "Unable to 'cd $testDir'."
