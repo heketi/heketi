cfb5672361e853b7323e032930545480020437fc
John Mulligan <jmulligan@redhat.com>
2018-01-31 16:30:12 -0500

    apps: expand the create volume operation stubs with real code
    Make the CreateVolumeOperation a real operation with the needed
    hooks back into the volume entry to create a volume. It implements
    the Build, Exec, Finalize or Rollback phases of the operation as
    well as the metadata hooks needed to expose the operation back
    to the API.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/operations.go
+++ apps/glusterfs/operations.go
@@ -1,7 +1,12 @@
 package glusterfs
 
 import (
+	"fmt"
+
+	"github.com/heketi/heketi/executors"
 	wdb "github.com/heketi/heketi/pkg/db"
+
+	"github.com/boltdb/bolt"
 )
 
 type Operation interface {
@@ -39,21 +44,111 @@ func NewVolumeCreateOperation(
 	}
 }
 
+func (vc *VolumeCreateOperation) Label() string {
+	return "Create Volume"
+}
+
+func (vc *VolumeCreateOperation) ResourceUrl() string {
+	return fmt.Sprintf("/volumes/%v", vc.vol.Info.Id)
+}
+
 func (vc *VolumeCreateOperation) Build(allocator Allocator) error {
-	// vc.vol.createVolumeComponents(vc.db, allocator)
-	return nil
+	return vc.db.Update(func(tx *bolt.Tx) error {
+		txdb := wdb.WrapTx(tx)
+		brick_entries, err := vc.vol.createVolumeComponents(txdb, allocator)
+		if err != nil {
+			return err
+		}
+		for _, brick := range brick_entries {
+			vc.op.RecordAddBrick(brick)
+			if e := brick.Save(tx); e != nil {
+				return e
+			}
+		}
+		vc.op.RecordAddVolume(vc.vol)
+		if e := vc.vol.Save(tx); e != nil {
+			return e
+		}
+		if e := vc.op.Save(tx); e != nil {
+			return e
+		}
+		return nil
+	})
 }
 
 func (vc *VolumeCreateOperation) Exec(executor executors.Executor) error {
-	// vc.vol.createVolumeExec(vc.db, executor)
-	return nil
+	brick_entries, err := bricksFromOp(vc.db, vc.op, vc.vol.Info.Gid)
+	if err != nil {
+		logger.LogError("Failed to get bricks from op: %v", err)
+		return err
+	}
+	err = vc.vol.createVolumeExec(vc.db, executor, brick_entries)
+	if err != nil {
+		logger.LogError("Error executing create volume: %v", err)
+	}
+	return err
 }
 
 func (vc *VolumeCreateOperation) Finalize() error {
-	return nil
+	return vc.db.Update(func(tx *bolt.Tx) error {
+		brick_entries, err := bricksFromOp(wdb.WrapTx(tx), vc.op, vc.vol.Info.Gid)
+		if err != nil {
+			logger.LogError("Failed to get bricks from op: %v", err)
+			return err
+		}
+		for _, brick := range brick_entries {
+			vc.op.FinalizeBrick(brick)
+			if e := brick.Save(tx); e != nil {
+				return e
+			}
+		}
+		vc.op.FinalizeVolume(vc.vol)
+		if e := vc.vol.Save(tx); e != nil {
+			return e
+		}
+
+		vc.op.Delete(tx)
+		return nil
+	})
 }
 
 func (vc *VolumeCreateOperation) Rollback(executor executors.Executor) error {
-	// vc.vol.cleanupCreateVolume(vc.db, executors, brick_entries)
-	return nil
+	// TODO make this into one transaction too
+	brick_entries, err := bricksFromOp(vc.db, vc.op, vc.vol.Info.Gid)
+	if err != nil {
+		logger.LogError("Failed to get bricks from op: %v", err)
+		return err
+	}
+	err = vc.vol.cleanupCreateVolume(vc.db, executor, brick_entries)
+	if err != nil {
+		logger.LogError("Error on create volume rollback: %v", err)
+		return err
+	}
+	err = vc.db.Update(func(tx *bolt.Tx) error {
+		return vc.op.Delete(tx)
+	})
+	return err
+}
+
+func bricksFromOp(db wdb.RODB,
+	op *PendingOperationEntry, gid int64) ([]*BrickEntry, error) {
+
+	brick_entries := []*BrickEntry{}
+	err := db.View(func(tx *bolt.Tx) error {
+		for _, a := range op.Actions {
+			if a.Change == OpAddBrick {
+				brick, err := NewBrickEntryFromId(tx, a.Id)
+				if err != nil {
+					return err
+				}
+				// this next line is a bit of an unfortunate hack because
+				// the db does not preserver the requested gid that is
+				// needed for the request
+				brick.gidRequested = gid
+				brick_entries = append(brick_entries, brick)
+			}
+		}
+		return nil
+	})
+	return brick_entries, err
 }
