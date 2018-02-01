87b464728b997cc23aa148314b00288cc537f922
John Mulligan <jmulligan@redhat.com>
2018-02-01 14:00:39 -0500

    apps: add test cases around create volume operation error handling
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/operations_test.go
+++ apps/glusterfs/operations_test.go
@@ -257,3 +257,128 @@ func TestVolumeCreatePendingNoSpace(t *testing.T) {
 		return nil
 	})
 }
+
+func TestVolumeCreatePendingBrickMissing(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	req := &api.VolumeCreateRequest{}
+	req.Size = 1024
+	req.Durability.Type = api.DurabilityReplicate
+	req.Durability.Replicate.Replica = 3
+
+	vol := NewVolumeEntryFromRequest(req)
+	vc := NewVolumeCreateOperation(vol, app.db)
+
+	// verify that there are no volumes, bricks or pending operations
+	app.db.View(func(tx *bolt.Tx) error {
+		vl, e := VolumeList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(vl) == 0, "expected len(vl) == 0, got", len(vl))
+		bl, e := BrickList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(bl) == 0, "expected len(bl) == 0, got", len(bl))
+		pol, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(pol) == 0, "expected len(pol) == 0, got", len(pol))
+		return nil
+	})
+
+	e := vc.Build(app.Allocator())
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	// verify volumes, bricks, & pending ops exist
+	app.db.View(func(tx *bolt.Tx) error {
+		vl, e := VolumeList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(vl) == 1, "expected len(vl) == 1, got", len(vl))
+		bl, e := BrickList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(bl) == 3, "expected len(bl) == 3, got", len(bl))
+		pol, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(pol) == 1, "expected len(pol) == 1, got", len(pol))
+
+		for _, vid := range vl {
+			v, e := NewVolumeEntryFromId(tx, vid)
+			tests.Assert(t, e == nil, "expected e == nil, got", e)
+			tests.Assert(t, v.Pending.Id == pol[0],
+				"expected v.Pending.Id == pol[0], got:", v.Pending.Id, pol[0])
+		}
+		for _, bid := range bl {
+			b, e := NewBrickEntryFromId(tx, bid)
+			tests.Assert(t, e == nil, "expected e == nil, got", e)
+			tests.Assert(t, b.Pending.Id == pol[0],
+				"expected b.Pending.Id == pol[0], got:", b.Pending.Id, pol[0])
+		}
+		return nil
+	})
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		bl, e := BrickList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		b, e := NewBrickEntryFromId(tx, bl[0])
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		e = b.Delete(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		return nil
+	})
+
+	// now that the brick list in the db is broken Exec/Finalize/Rollback
+	// will return errors
+
+	e = vc.Exec(app.executor)
+	tests.Assert(t, e != nil, "expected e != nil, got", e)
+
+	e = vc.Finalize()
+	tests.Assert(t, e != nil, "expected e != nil, got", e)
+
+	e = vc.Rollback(app.executor)
+	tests.Assert(t, e != nil, "expected e != nil, got", e)
+}
+
+func TestVolumeCreateOperationBasics(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	req := &api.VolumeCreateRequest{}
+	req.Size = 1024
+	req.Durability.Type = api.DurabilityReplicate
+	req.Durability.Replicate.Replica = 3
+
+	vol := NewVolumeEntryFromRequest(req)
+	vol.Info.Id = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	vc := NewVolumeCreateOperation(vol, app.db)
+
+	tests.Assert(t, vc.Id() == vc.op.Id,
+		"expected vc.Id() == vc.op.Id, got:", vc.Id(), vc.op.Id)
+	tests.Assert(t, vc.Label() == "Create Volume",
+		`expected vc.Label() == "Volume Create", got:`, vc.Label())
+	tests.Assert(t, vc.ResourceUrl() == "/volumes/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+		`expected vc.ResourceUrl() == "/volumes/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", got:`,
+		vc.ResourceUrl())
+}
