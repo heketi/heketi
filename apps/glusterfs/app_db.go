3b77eb67bfbb9a15ddaddd8acf02c3c7ba549c7f
John Mulligan <jmulligan@redhat.com>
2018-02-12 13:03:14 -0500

    apps: use common db bucket initializer in app_db.go
    Now that we have a common function for setting up the db buckets
    use it in the app_db.go file.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/app_db.go
+++ apps/glusterfs/app_db.go
@@ -283,55 +283,7 @@ func DbCreate(jsonfile string, dbfile string, debug bool) error {
 	}
 
 	err = dbhandle.Update(func(tx *bolt.Tx) error {
-		// Create Cluster Bucket
-		_, err := tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_CLUSTER))
-		if err != nil {
-			logger.Debug("Unable to create cluster bucket in DB")
-			return err
-		}
-
-		// Create Node Bucket
-		_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_NODE))
-		if err != nil {
-			logger.Debug("Unable to create node bucket in DB")
-			return err
-		}
-
-		// Create Volume Bucket
-		_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_VOLUME))
-		if err != nil {
-			logger.Debug("Unable to create volume bucket in DB")
-			return err
-		}
-
-		// Create Device Bucket
-		_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_DEVICE))
-		if err != nil {
-			logger.Debug("Unable to create device bucket in DB")
-			return err
-		}
-
-		// Create Brick Bucket
-		_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_BRICK))
-		if err != nil {
-			logger.Debug("Unable to create brick bucket in DB")
-			return err
-		}
-
-		_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_BLOCKVOLUME))
-		if err != nil {
-			logger.Debug("Unable to create blockvolume bucket in DB")
-			return err
-		}
-
-		_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_DBATTRIBUTE))
-		if err != nil {
-			logger.Debug("Unable to create dbattribute bucket in DB")
-			return err
-		}
-
-		return nil
-
+		return initializeBuckets(tx)
 	})
 	if err != nil {
 		logger.Err(err)
