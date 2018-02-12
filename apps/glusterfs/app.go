af8f0df1f7ac73a97776de5544466c3539abafc0
John Mulligan <jmulligan@redhat.com>
2018-02-12 12:58:21 -0500

    apps: create a common function for initializing db buckets
    Currently we have two sections of code that initialize db buckets.
    This change moves the code respnosible for setting up the
    buckets from app.go to a new function in dbcommon.go
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/app.go
+++ apps/glusterfs/app.go
@@ -113,50 +113,9 @@ func NewApp(configIo io.Reader) *App {
 		app.dbReadOnly = true
 	} else {
 		err = app.db.Update(func(tx *bolt.Tx) error {
-			// Create Cluster Bucket
-			_, err := tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_CLUSTER))
+			err := initializeBuckets(tx)
 			if err != nil {
-				logger.LogError("Unable to create cluster bucket in DB")
-				return err
-			}
-
-			// Create Node Bucket
-			_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_NODE))
-			if err != nil {
-				logger.LogError("Unable to create node bucket in DB")
-				return err
-			}
-
-			// Create Volume Bucket
-			_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_VOLUME))
-			if err != nil {
-				logger.LogError("Unable to create volume bucket in DB")
-				return err
-			}
-
-			// Create Device Bucket
-			_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_DEVICE))
-			if err != nil {
-				logger.LogError("Unable to create device bucket in DB")
-				return err
-			}
-
-			// Create Brick Bucket
-			_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_BRICK))
-			if err != nil {
-				logger.LogError("Unable to create brick bucket in DB")
-				return err
-			}
-
-			_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_BLOCKVOLUME))
-			if err != nil {
-				logger.LogError("Unable to create blockvolume bucket in DB")
-				return err
-			}
-
-			_, err = tx.CreateBucketIfNotExists([]byte(BOLTDB_BUCKET_DBATTRIBUTE))
-			if err != nil {
-				logger.LogError("Unable to create dbattribute bucket in DB")
+				logger.LogError("Unable to initialize buckets")
 				return err
 			}
 
