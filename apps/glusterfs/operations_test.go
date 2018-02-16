428a1921a6e5b3720fe2cdb6a6d0bb85a8eeb0c3
John Mulligan <jmulligan@redhat.com>
2018-02-15 19:06:57 -0500

    apps: add a test for volume expand operation
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/operations_test.go
+++ apps/glusterfs/operations_test.go
@@ -457,6 +457,97 @@ func TestVolumeDeleteOperation(t *testing.T) {
 	})
 }
 
+func TestVolumeExpandOperation(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	req := &api.VolumeCreateRequest{}
+	req.Size = 1024
+	req.Durability.Type = api.DurabilityReplicate
+	req.Durability.Replicate.Replica = 3
+
+	// first we need to create a volume to delete
+	vol := NewVolumeEntryFromRequest(req)
+	vc := NewVolumeCreateOperation(vol, app.db)
+
+	e := vc.Build(app.Allocator())
+	tests.Assert(t, e == nil, "expected e == nil, got:", e)
+	e = vc.Exec(app.executor)
+	tests.Assert(t, e == nil, "expected e == nil, got:", e)
+	e = vc.Finalize()
+	tests.Assert(t, e == nil, "expected e == nil, got:", e)
+
+	app.db.View(func(tx *bolt.Tx) error {
+		bl, e := BrickList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(bl) == 3, "expected len(bl) == 3, got:", len(bl))
+		po, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(po) == 0, "expected len(po) == 0, got:", len(po))
+		return nil
+	})
+
+	ve := NewVolumeExpandOperation(vol, app.db, 100)
+	e = ve.Build(app.Allocator())
+	tests.Assert(t, e == nil, "expected e == nil, got:", e)
+
+	app.db.View(func(tx *bolt.Tx) error {
+		bl, e := BrickList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(bl) == 6, "expected len(bl) == 6, got:", len(bl))
+		pcount := 0
+		for _, id := range bl {
+			b, e := NewBrickEntryFromId(tx, id)
+			tests.Assert(t, e == nil, "expected e == nil, got", e)
+			if b.Pending.Id != "" {
+				pcount++
+			}
+		}
+		tests.Assert(t, pcount == 3, "expected len(bl) == 3, got:", pcount)
+		po, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(po) == 1, "expected len(po) == 1, got:", len(po))
+		return nil
+	})
+
+	e = ve.Exec(app.executor)
+	tests.Assert(t, e == nil, "expected e == nil, got:", e)
+
+	e = ve.Finalize()
+	tests.Assert(t, e == nil, "expected e == nil, got:", e)
+
+	app.db.View(func(tx *bolt.Tx) error {
+		bl, e := BrickList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(bl) == 6, "expected len(bl) == 6, got:", len(bl))
+		pcount := 0
+		for _, id := range bl {
+			b, e := NewBrickEntryFromId(tx, id)
+			tests.Assert(t, e == nil, "expected e == nil, got", e)
+			if b.Pending.Id != "" {
+				pcount++
+			}
+		}
+		tests.Assert(t, pcount == 0, "expected len(bl) == 0, got:", pcount)
+		po, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(po) == 0, "expected len(po) == 0, got:", len(po))
+		return nil
+	})
+}
+
 func TestDeviceRemoveOperationEmpty(t *testing.T) {
 	tmpfile := tests.Tempfile()
 	defer os.Remove(tmpfile)
