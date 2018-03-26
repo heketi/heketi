529814d9f888af430f66f02ea3a96a71ae5054da
John Mulligan <jmulligan@redhat.com>
2018-03-26 10:51:54 -0400

    apps: correctly update the hosting volume during block volume create
    The block hosting volume was getting flagged with pending metadata but
    not saved back to the db, thus the volume did not appear as pending
    in the db when it should have been.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/operations.go
+++ apps/glusterfs/operations.go
@@ -507,7 +507,7 @@ func (bvc *BlockVolumeCreateOperation) Build() error {
 				}
 			}
 			bvc.op.RecordAddHostingVolume(vol)
-			if e := bvc.bvol.Save(tx); e != nil {
+			if e := vol.Save(tx); e != nil {
 				return e
 			}
 			bvc.bvol.Info.BlockHostingVolume = vol.Info.Id
