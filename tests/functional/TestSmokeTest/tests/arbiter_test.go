e8811284d2add6d3d59ad07101763bbc72722764
John Mulligan <jmulligan@redhat.com>
2018-04-02 16:30:50 -0400

    functional tests: add tests for node tagging & arbiter
    Add some tests that exercise the new tagging support and the arbiter
    specific tags.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/functional/TestSmokeTest/tests/arbiter_test.go
+++ tests/functional/TestSmokeTest/tests/arbiter_test.go
@@ -76,6 +76,123 @@ func TestArbiterFlatCluster(t *testing.T) {
 	t.Run("testArbiterReplaceArbiterBrick", testArbiterReplaceArbiterBrick)
 }
 
+func TestArbiterTagging(t *testing.T) {
+	setupCluster(t, 4, 8)
+	defer teardownCluster(t)
+
+	cl, err := heketi.ClusterList()
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+	tests.Assert(t, len(cl.Clusters) > 0,
+		"expected len(cl.Clusters) > 0, got:", len(cl.Clusters))
+	clusterId := cl.Clusters[0]
+
+	clusterInfo, err := heketi.ClusterInfo(clusterId)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	nid := clusterInfo.Nodes[0]
+	n, err := heketi.NodeInfo(nid)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	tests.Assert(t, len(n.Tags) == 0,
+		"expected len(n.Tags) == 0, got:", len(n.Tags))
+
+	err = heketi.NodeSetTags(nid, &api.TagsChangeRequest{
+		SetTags: map[string]string{
+			"flourpower": "300",
+			"zamboni":    "yes please",
+		},
+	})
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	n, err = heketi.NodeInfo(nid)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+	tests.Assert(t, len(n.Tags) == 2,
+		"expected len(n.Tags) == 2, got:", len(n.Tags))
+
+	err = heketi.NodeSetTags(nid, &api.TagsChangeRequest{})
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	n, err = heketi.NodeInfo(nid)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+	tests.Assert(t, len(n.Tags) == 0,
+		"expected len(n.Tags) == 0, got:", len(n.Tags))
+
+	err = heketi.NodeSetTags(nid, &api.TagsChangeRequest{
+		SetTags: map[string]string{
+			"arbiter": "disabled",
+		},
+	})
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	n, err = heketi.NodeInfo(nid)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+	tests.Assert(t, len(n.Tags) == 1,
+		"expected len(n.Tags) == 1, got:", len(n.Tags))
+}
+
+func TestArbiterTaggedNodes(t *testing.T) {
+	setupCluster(t, 4, 8)
+	defer teardownCluster(t)
+
+	// first we're going to test that we cannot create
+	// an arbiter volume if none of the devices support arbiter
+	cl, err := heketi.ClusterList()
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+	tests.Assert(t, len(cl.Clusters) > 0,
+		"expected len(cl.Clusters) > 0, got:", len(cl.Clusters))
+	clusterId := cl.Clusters[0]
+
+	clusterInfo, err := heketi.ClusterInfo(clusterId)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	for _, nid := range clusterInfo.Nodes {
+		n, err := heketi.NodeInfo(nid)
+		tests.Assert(t, err == nil, "expected err == nil, got:", err)
+		tests.Assert(t, len(n.Tags) == 0,
+			"expected len(n.Tags) == 0, got:", len(n.Tags))
+
+		err = heketi.NodeSetTags(nid, &api.TagsChangeRequest{
+			SetTags: map[string]string{
+				"arbiter": "disabled",
+			},
+		})
+		tests.Assert(t, err == nil, "expected err == nil, got:", err)
+	}
+
+	volReq := &api.VolumeCreateRequest{}
+	volReq.Size = 10
+	volReq.Durability.Type = api.DurabilityReplicate
+	volReq.Durability.Replicate.Replica = 3
+	volReq.GlusterVolumeOptions = []string{"user.heketi.arbiter true"}
+
+	_, err = heketi.VolumeCreate(volReq)
+	tests.Assert(t, err != nil, "expected err != nil, got:", err)
+
+	vl, err := heketi.VolumeList()
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+	tests.Assert(t, len(vl.Volumes) == 0,
+		"expected len(vl.Volumes) == 0, got:", len(vl.Volumes))
+
+	// now tag one of the nodes for arbiter, and things should work
+	nid := clusterInfo.Nodes[0]
+	err = heketi.NodeSetTags(nid, &api.TagsChangeRequest{
+		SetTags: map[string]string{
+			"arbiter": "required",
+		},
+	})
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	t.Run("testArbiterCreateSimple", testArbiterCreateSimple)
+	teardownVolumes(t)
+	t.Run("testArbiterCreateAndVerify", testArbiterCreateAndVerify)
+	teardownVolumes(t)
+	t.Run("testNonArbiterIsNotArbiter", testNonArbiterIsNotArbiter)
+	teardownVolumes(t)
+	t.Run("testArbiterReplaceDataBrick", testArbiterReplaceDataBrick)
+	teardownVolumes(t)
+	t.Run("testArbiterReplaceArbiterBrick", testArbiterReplaceArbiterBrick)
+}
+
 func testArbiterCreateSimple(t *testing.T) {
 	vl, err := heketi.VolumeList()
 	tests.Assert(t, err == nil, "expected err == nil, got:", err)
