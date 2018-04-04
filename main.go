1960b3cced09045f0f4bbc0730ef60ecaf8894f5
Madhu Rajanna <mrajanna@redhat.com>
2018-04-04 20:26:10 +0530

    heketi:main start using throttling middleware in heketi
    added configuration for handling http
    throttling
    
    Co-authored-by: Raghavendra Talur <rtalur@redhat.com>
    Signed-off-by: Madhu Rajanna <mrajanna@redhat.com>

--- main.go
+++ main.go
@@ -31,6 +31,8 @@ type Config struct {
 	AuthEnabled          bool                     `json:"use_auth"`
 	JwtConfig            middleware.JwtAuthConfig `json:"jwt"`
 	BackupDbToKubeSecret bool                     `json:"backup_db_to_kube_secret"`
+	HTTPThrottleEnabled  bool                     `json:"use_http_throttle"`
+	MaxHTTPThrottleCount uint32                   `json:"max_http_throttle_count"`
 }
 
 var (
@@ -367,6 +369,19 @@ func main() {
 		n.UseFunc(app.Auth)
 
 		fmt.Println("Authorization loaded")
+	}
+	//Load HTTP request Throttling Middleware
+	if options.HTTPThrottleEnabled {
+		maxHTTPThrottleCount := options.MaxHTTPThrottleCount
+		if maxHTTPThrottleCount == 0 {
+			maxHTTPThrottleCount = 30
+		}
+		HTTPThrottle := middleware.NewHTTPThrottler(maxHTTPThrottleCount)
+
+		n.Use(HTTPThrottle)
+
+		//clean up required if we wont get GET request after POST/DELETE to check status
+		go HTTPThrottle.Cleanup()
 	}
 
 	if options.BackupDbToKubeSecret {
