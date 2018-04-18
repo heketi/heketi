e7c126baede406377aa2f6fa9cf21ca4ec18b722
Madhu Rajanna <mrajanna@redhat.com>
2018-04-18 11:56:59 +0530

    middleware:http_throttling_test new testcases
    added new test cases and updated the testcases
    
    Signed-off-by: Madhu Rajanna <mrajanna@redhat.com>

--- middleware/http_throttling_test.go
+++ middleware/http_throttling_test.go
@@ -12,7 +12,6 @@ package middleware
 import (
 	"bytes"
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -34,10 +33,19 @@ func TestReachedMaxRequest(t *testing.T) {
 	nt := NewHTTPThrottler(10)
 	tests.Assert(t, nt.reachedMaxRequest() == false)
 	nt.servingCount = 10
+	nt.reqRecvCount = 10
 	tests.Assert(t, nt.reachedMaxRequest() == true)
+	tests.Assert(t, nt.reqReceivedcount() == true)
 
 }
 
+func TestCheckIDPresent(t *testing.T) {
+	nt := NewHTTPThrottler(10)
+	nt.requestCache["test"] = time.Now()
+	tests.Assert(t, nt.checkReqIDPresent("test") == true)
+	tests.Assert(t, nt.checkReqIDPresent("") == false)
+
+}
 func TestIsSuccess(t *testing.T) {
 	s := isSuccess(200)
 	tests.Assert(t, s == true)
@@ -64,9 +72,11 @@ func TestServeHTTP(t *testing.T) {
 
 func TestServeHTTPDelete(t *testing.T) {
 	nt := NewHTTPThrottler(10)
+	rt := &RequestID{}
 	tests.Assert(t, nt != nil)
 
-	n := negroni.New(nt)
+	n := negroni.New(rt)
+	n.Use(nt)
 	n.UseHandlerFunc(mw)
 	client := http.Client{}
 	ts := httptest.NewServer(n)
@@ -81,8 +91,10 @@ func TestServeHTTPDelete(t *testing.T) {
 
 func TestServeHTTPTrottleTomanyReq(t *testing.T) {
 	nt := NewHTTPThrottler(9)
+	rt := &RequestID{}
 	tests.Assert(t, nt != nil)
-	n := negroni.New(nt)
+	n := negroni.New(rt)
+	n.Use(nt)
 	n.UseHandlerFunc(mw)
 	client := http.Client{}
 	ts := httptest.NewServer(n)
@@ -94,20 +106,22 @@ func TestServeHTTPTrottleTomanyReq(t *testing.T) {
 			client.Do(r)
 		}
 
-	}(10)
+	}(9)
 
 	r, err := http.NewRequest("DELETE", ts.URL, nil)
 	tests.Assert(t, err == nil)
 	resp, err := client.Do(r)
 	tests.Assert(t, err == nil)
-	tests.Assert(t, resp.StatusCode == http.StatusTooManyRequests)
+	tests.Assert(t, resp.StatusCode == http.StatusTooManyRequests, `expected resp.StatusCode == http.StatusTooManyRequests, got:`, resp.StatusCode)
 
 }
 
 func TestServeHTTPTrottle(t *testing.T) {
 	nt := NewHTTPThrottler(10)
+	rt := &RequestID{}
 	tests.Assert(t, nt != nil)
-	n := negroni.New(nt)
+	n := negroni.New(rt)
+	n.Use(nt)
 
 	n.UseHandlerFunc(mw)
 	client := http.Client{}
@@ -117,8 +131,10 @@ func TestServeHTTPTrottle(t *testing.T) {
 	func(n int) {
 		for i := 0; i < n; i++ {
 			uid, r := createReq("POST", ts.URL)
-			client.Do(r)
+			resp, _ := client.Do(r)
+			uid = resp.Header.Get("X-Request-ID")
 			http.Get(ts.URL + "/volume/" + uid)
+
 		}
 
 	}(20)
@@ -133,9 +149,10 @@ func TestServeHTTPTrottle(t *testing.T) {
 
 func TestServeHTTPTrottleQueue(t *testing.T) {
 	nt := NewHTTPThrottler(10)
+	rt := &RequestID{}
 	tests.Assert(t, nt != nil)
-	n := negroni.New(nt)
-
+	n := negroni.New(rt)
+	n.Use(nt)
 	n.UseHandlerFunc(mw)
 	client := http.Client{}
 	ts := httptest.NewServer(n)
@@ -144,7 +161,8 @@ func TestServeHTTPTrottleQueue(t *testing.T) {
 	func(n int) {
 		for i := 0; i < n; i++ {
 			uid, r := createReq("POST", ts.URL)
-			client.Do(r)
+			resp, _ := client.Do(r)
+			uid = resp.Header.Get("X-Request-ID")
 			url = ts.URL + "/volume/" + uid
 		}
 
@@ -165,8 +183,10 @@ func TestServeHTTPTrottleQueue(t *testing.T) {
 func TestThrottlingCleanup(t *testing.T) {
 
 	nt := NewHTTPThrottler(10)
+	rt := &RequestID{}
 	tests.Assert(t, nt != nil)
-	n := negroni.New(nt)
+	n := negroni.New(rt)
+	n.Use(nt)
 	tt := time.Now()
 	throttleNow = func() time.Time { return tt }
 	n.UseHandlerFunc(mw)
@@ -201,12 +221,10 @@ var createReq = func(method, url string) (string, *http.Request) {
 var mw = func(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost, http.MethodDelete:
-		body, _ := ioutil.ReadAll(r.Body)
-		data := make(map[string]string)
-		json.Unmarshal(body, &data)
-		rw.Header().Set("X-Request-ID", data["RequestId"])
 		rw.Header().Set("X-Pending", "true")
+		rw.Header().Add("X-Request-ID", GetRequestID(r.Context()))
 		rw.WriteHeader(http.StatusAccepted)
+
 	case http.MethodGet:
 		rw.WriteHeader(http.StatusOK)
 
