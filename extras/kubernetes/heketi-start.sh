46c781920c4982235f56b3e5bf1876cfeda633d9
Artem Teleshev <artem.teleshev@gmail.com>
2018-04-25 16:47:31 +0300

    Allow pass the database file name via env var to heketi-start.sh
    Now heketi provides ability to change path of db file in the server configuration file (heketi.json)
    But this ability didn't provided in heketi-start.sh
    This small fix gives this ability.
    In pod spec can be specified environment variable with another path to db file: HEKETI_DB_PATH=/my/path/to/heketi.db
    
    Signed-off-by: Artem Teleshev <artem.teleshev@gmail.com>

--- extras/kubernetes/heketi-start.sh
+++ extras/kubernetes/heketi-start.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
+HEKETI_DB_PATH=${HEKETI_DB_PATH:-/var/lib/heketi/heketi.db}
+
 if [ -f /backupdb/heketi.db.gz ] ; then
-    gunzip -c /backupdb/heketi.db.gz > /var/lib/heketi/heketi.db
+    gunzip -c /backupdb/heketi.db.gz > "${HEKETI_DB_PATH}"
     if [ $? -ne 0 ] ; then
         echo "Unable to copy database"
         exit 1
     fi
-    echo "Copied backup db to /var/lib/heketi/heketi.db"
+    echo "Copied backup db to ${HEKETI_DB_PATH}"
 elif [ -f /backupdb/heketi.db ] ; then
-    cp /backupdb/heketi.db /var/lib/heketi/heketi.db
+    cp /backupdb/heketi.db "${HEKETI_DB_PATH}"
     if [ $? -ne 0 ] ; then
         echo "Unable to copy database"
         exit 1
     fi
-    echo "Copied backup db to /var/lib/heketi/heketi.db"
+    echo "Copied backup db to ${HEKETI_DB_PATH}"
 fi
 
 /usr/bin/heketi --config=/etc/heketi/heketi.json
