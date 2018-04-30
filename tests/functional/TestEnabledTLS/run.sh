a06f1ed3c86cb2bde38f8eebafc81a1a4b80b8de
John Mulligan <jmulligan@redhat.com>
2018-04-30 10:52:33 -0400

    functional tests: ensure openssl command generated files in TLS test
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/functional/TestEnabledTLS/run.sh
+++ tests/functional/TestEnabledTLS/run.sh
@@ -10,6 +10,7 @@ cd "$SCRIPT_DIR" || exit 1
 
 cp "$HEKETI_DIR/heketi" "$HEKETI_SERVER"
 
+rm -f heketi.key heketi.crt
 openssl req \
     -newkey rsa:2048 \
     -x509 \
@@ -21,6 +22,14 @@ openssl req \
     -extensions alt_names \
     -config ssl.conf \
     -days 3650
+
+for fn in "heketi.key" "heketi.crt"; do
+    if [[ ! -f "$fn" ]]; then
+        echo "ERROR: openssl failed to create ${fn} file" >&2
+        exit 1
+    fi
+done
+
 
 if ! command -v virtualenv &>/dev/null; then
     echo "WARNING: virtualenv not installed... skipping test" >&2
