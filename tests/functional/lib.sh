1f94af23def79ff941a0907585180773ecc96fe9
John Mulligan <jmulligan@redhat.com>
2018-04-30 09:44:59 -0400

    functional tests: attempt to more reliably kill heketi server
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/functional/lib.sh
+++ tests/functional/lib.sh
@@ -19,6 +19,7 @@ _sudo() {
 
 HEKETI_PID=
 start_heketi() {
+    HEKETI_PID=
     ( cd "$HEKETI_SERVER_BUILD_DIR" && make && cp heketi "$HEKETI_SERVER" )
     if [ $? -ne 0 ] ; then
         fail "Unable to build Heketi"
@@ -29,6 +30,25 @@ start_heketi() {
     $HEKETI_SERVER --config=config/heketi.json &
     HEKETI_PID=$!
     sleep 2
+}
+
+stop_heketi() {
+    if [[ -z "$HEKETI_PID" ]]; then
+        # heketi pid was not set, nothing to stop
+        return 0
+    fi
+
+    kill "$HEKETI_PID"
+    sleep 0.2
+    for i in $(seq 1 5); do
+        if [[ ! -d "/proc/${HEKETI_PID}" ]]; then
+            break
+        fi
+        echo "WARNING: Heketi server may still be running."
+        ps -f "$HEKETI_PID"
+        kill "$HEKETI_PID"
+        sleep 1
+    done
 }
 
 start_vagrant() {
@@ -94,7 +114,7 @@ functional_tests() {
     run_go_tests
     pause_test "$HEKETI_TEST_PAUSE_AFTER"
 
-    kill $HEKETI_PID
+    stop_heketi
     if [[ "$HEKETI_TEST_CLEANUP" != "no" ]]
     then
         teardown
