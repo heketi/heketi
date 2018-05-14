b177b6e44467eb3da86864f9a78272d33cfb84a3
John Mulligan <jmulligan@redhat.com>
2018-05-14 11:02:14 -0400

    apps: correctly handle space reclaimed on brick destroy in replace path
    Now that cloning has made it possible to share storage of multiple
    bricks we have to cope with that fact in the device remove / brick
    replace path. The refactoring of the function stubbed the new
    return values of Destroy in, but did nothing with the space reclaimed
    flag. This change makes the replace brick function correctly
    take that into account.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/volume_entry_allocate.go
+++ apps/glusterfs/volume_entry_allocate.go
@@ -382,7 +382,10 @@ func (v *VolumeEntry) replaceBrickInVolume(db wdb.DB, executor executors.Executo
 	// After this point we should not call any defer func()
 	// We don't have a *revert* of replace brick operation
 
-	_, _ = oldBrickEntry.Destroy(db, executor)
+	spaceReclaimed, err := oldBrickEntry.Destroy(db, executor)
+	if err != nil {
+		logger.LogError("Error destroying old brick: %v", err)
+	}
 
 	// We must read entries from db again as state on disk might
 	// have changed
@@ -400,6 +403,17 @@ func (v *VolumeEntry) replaceBrickInVolume(db wdb.DB, executor executors.Executo
 		err = reReadNewDeviceEntry.Save(tx)
 		if err != nil {
 			return err
+		}
+		if spaceReclaimed {
+			oldDevice2, err := NewDeviceEntryFromId(tx, oldBrickEntry.Info.DeviceId)
+			if err != nil {
+				return err
+			}
+			oldDevice2.StorageFree(oldBrickEntry.TotalSize())
+			err = oldDevice2.Save(tx)
+			if err != nil {
+				return err
+			}
 		}
 
 		reReadVolEntry, err := NewVolumeEntryFromId(tx, newBrickEntry.Info.VolumeId)
