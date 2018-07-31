ace398005eafca6b781c89d188df51038c7839a5
John Mulligan <jmulligan@redhat.com>
2018-07-31 15:33:53 -0400

    functional tests: add an env var that skips starting server
    Add an environment variable (HEKETI_TEST_SERVER) that when set to "no"
    will prevent the functions in lib.sh from starting the heketi server.
    This allows the use of the lib.sh functions for setting up the
    environment but allows the test cases to control the starting and
    stopping of the server. This will be used to write tests that exercise
    the error injection executor.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/functional/lib.sh
+++ tests/functional/lib.sh
@@ -28,13 +28,17 @@ wait_for_heketi() {
     return 1
 }
 
-HEKETI_PID=
-start_heketi() {
-    HEKETI_PID=
-    ( cd "$HEKETI_SERVER_BUILD_DIR" && make && cp heketi "$HEKETI_SERVER" )
+build_heketi() {
+    ( cd "$HEKETI_SERVER_BUILD_DIR" && make server && cp heketi "$HEKETI_SERVER" )
     if [ $? -ne 0 ] ; then
         fail "Unable to build Heketi"
     fi
+}
+
+HEKETI_PID=
+start_heketi() {
+    HEKETI_PID=
+    build_heketi
 
     # Start server
     rm -f heketi.db > /dev/null 2>&1
@@ -126,7 +130,13 @@ functional_tests() {
     then
         start_vagrant
     fi
-    start_heketi
+    if [[ "$HEKETI_TEST_SERVER" == "no" ]]; then
+        build_heketi
+        # make sure pid is unset so stop_heketi does nothing
+        HEKETI_PID=
+    else
+        start_heketi
+    fi
 
     pause_test "$HEKETI_TEST_PAUSE_BEFORE"
     run_go_tests
