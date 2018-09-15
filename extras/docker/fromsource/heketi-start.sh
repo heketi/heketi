c29560d711f17fab34fd30beadef9762c39b544c
John Mulligan <jmulligan@redhat.com>
2018-09-15 12:15:35 -0400

    extras: flatten logic in heketi-start.sh
    Flatten if-else branch that ensures that db checks are only done
    if the data dir exists. Instead, unconditionally create the data
    dir and fail if the dir does not exist.
    
    (May be best viewed with diff -w)
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- extras/docker/fromsource/heketi-start.sh
+++ extras/docker/fromsource/heketi-start.sh
@@ -18,55 +18,54 @@ error() {
 
 info "Setting up heketi database"
 
-if [[ -d "${HEKETI_PATH}" ]]; then
-    # Test that our volume is writable.
-    touch "${HEKETI_PATH}/test" && rm "${HEKETI_PATH}/test"
-    if [ $? -ne 0 ]; then
-        error "${HEKETI_PATH} is read-only"
-        exit 1
-    fi
+# Ensure the data dir exists
+mkdir -p "${HEKETI_PATH}" 2>/dev/null
+if [[ $? -ne 0 && ! -d "${HEKETI_PATH}" ]]; then
+    error "Failed to create ${HEKETI_PATH}"
+    exit 1
+fi
 
-    if [[ ! -f "${HEKETI_PATH}/heketi.db" ]]; then
-        info "No database file found"
-        out=$(mount | grep "${HEKETI_PATH}" | grep heketidbstorage)
-        if [[ $? -eq 0 ]]; then
-            info "Database volume found: ${out}"
-            info "Database file is expected, waiting..."
-            check=0
-            while [[ ! -f "${HEKETI_PATH}/heketi.db" ]]; do
-                sleep 5
-                if [[ ${check} -eq 5 ]]; then
-                   error "Database file did not appear, exiting."
-                   exit 1
-                fi
-                ((check+=1))
-            done
-        fi
-    fi
+# Test that our volume is writable.
+touch "${HEKETI_PATH}/test" && rm "${HEKETI_PATH}/test"
+if [ $? -ne 0 ]; then
+    error "${HEKETI_PATH} is read-only"
+    exit 1
+fi
 
-    stat "${HEKETI_PATH}/heketi.db" | tee -a "${LOG}"
-    # Workaround for scenario where a lock on the heketi.db has not been
-    # released.
-    # This code uses a non-blocking flock in a loop rather than a blocking
-    # lock with timeout due to issues with current gluster and flock
-    # ( see rhbz#1613260 )
-    for _ in $(seq 1 60); do
-        flock --nonblock "${HEKETI_PATH}/heketi.db" true
-        flock_status=$?
-        if [[ $flock_status -eq 0 ]]; then
-            break
-        fi
-        sleep 1
-    done
-    if [[ $flock_status -ne 0 ]]; then
-        error "Database file is read-only"
+if [[ ! -f "${HEKETI_PATH}/heketi.db" ]]; then
+    info "No database file found"
+    out=$(mount | grep "${HEKETI_PATH}" | grep heketidbstorage)
+    if [[ $? -eq 0 ]]; then
+        info "Database volume found: ${out}"
+        info "Database file is expected, waiting..."
+        check=0
+        while [[ ! -f "${HEKETI_PATH}/heketi.db" ]]; do
+            sleep 5
+            if [[ ${check} -eq 5 ]]; then
+               error "Database file did not appear, exiting."
+               exit 1
+            fi
+            ((check+=1))
+        done
     fi
-else
-    mkdir -p "${HEKETI_PATH}"
-    if [[ $? -ne 0 ]]; then
-        error "Failed to create ${HEKETI_PATH}"
-        exit 1
+fi
+
+stat "${HEKETI_PATH}/heketi.db" | tee -a "${LOG}"
+# Workaround for scenario where a lock on the heketi.db has not been
+# released.
+# This code uses a non-blocking flock in a loop rather than a blocking
+# lock with timeout due to issues with current gluster and flock
+# ( see rhbz#1613260 )
+for _ in $(seq 1 60); do
+    flock --nonblock "${HEKETI_PATH}/heketi.db" true
+    flock_status=$?
+    if [[ $flock_status -eq 0 ]]; then
+        break
     fi
+    sleep 1
+done
+if [[ $flock_status -ne 0 ]]; then
+    error "Database file is read-only"
 fi
 
 if [[ -d "${BACKUPDB_PATH}" ]]; then
