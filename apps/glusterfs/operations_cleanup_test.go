60d11b35f2d92478dfd98a75ea2da3614aece106
John Mulligan <jmulligan@redhat.com>
2018-11-13 16:57:07 -0500

    apps: add a test for cleaning multiple operations
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/operations_cleanup_test.go
+++ apps/glusterfs/operations_cleanup_test.go
@@ -70,3 +70,80 @@ func TestBasicOperationsCleanup(t *testing.T) {
 		return nil
 	})
 }
+
+func TestOperationsCleanupThreeOps(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	req := &api.VolumeCreateRequest{}
+	req.Size = 1024
+	req.Durability.Type = api.DurabilityReplicate
+	req.Durability.Replicate.Replica = 3
+
+	// create a volume we can delete later
+	vol := NewVolumeEntryFromRequest(req)
+	vc := NewVolumeCreateOperation(vol, app.db)
+	e := RunOperation(vc, app.executor)
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 0, "expected len(l) == 0, got:", len(l))
+		return nil
+	})
+	dvol := vol
+
+	// create 1st pending op
+	vol = NewVolumeEntryFromRequest(req)
+	vc = NewVolumeCreateOperation(vol, app.db)
+	e = vc.Build()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	// create 2nd pending op
+	vol = NewVolumeEntryFromRequest(req)
+	vc = NewVolumeCreateOperation(vol, app.db)
+	e = vc.Build()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	// create 3rd pending op
+	vdel := NewVolumeDeleteOperation(dvol, app.db)
+	e = vdel.Build()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 3, "expected len(l) == 3, got:", len(l))
+		e = MarkPendingOperationsStale(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		return nil
+	})
+
+	oc := OperationCleaner{
+		db:       app.db,
+		executor: app.executor,
+		sel:      CleanAll,
+	}
+	e = oc.Clean()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.View(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 0, "expected len(l) == 0, got:", len(l))
+		return nil
+	})
+}
