51428bd2f4e788094b42db6a5850470d530233c1
John Mulligan <jmulligan@redhat.com>
2018-11-14 14:22:40 -0500

    apps: add test to verify non-cleanable ops don't get cleaned
    Add a test to verify that a non-cleanable op such as our
    device related ops don't get touched by the cleaner.
    For now these ops will be un-cleanable because they don't
    "replay nice" and need to have a human look at them.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/operations_cleanup_test.go
+++ apps/glusterfs/operations_cleanup_test.go
@@ -147,3 +147,80 @@ func TestOperationsCleanupThreeOps(t *testing.T) {
 		return nil
 	})
 }
+
+func TestOperationsCleanupSkipNonLoadable(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	// create a dummy volume so there's at least one brick on a device
+	req := &api.VolumeCreateRequest{}
+	req.Size = 1024
+	req.Durability.Type = api.DurabilityReplicate
+	req.Durability.Replicate.Replica = 3
+
+	// create a volume we can delete later
+	vol := NewVolumeEntryFromRequest(req)
+	vc := NewVolumeCreateOperation(vol, app.db)
+	e := RunOperation(vc, app.executor)
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	var deviceId string
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 0, "expected len(l) == 0, got:", len(l))
+		dl, e := DeviceList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(dl) > 1, "expected len(dl) > 1, got:", len(dl))
+		for _, d := range dl {
+			dev, e := NewDeviceEntryFromId(tx, d)
+			tests.Assert(t, e == nil, "expected e == nil, got", e)
+			if len(dev.Bricks) >= 1 {
+				deviceId = d
+			}
+		}
+		tests.Assert(t, deviceId != "")
+		return nil
+	})
+
+	dro := NewDeviceRemoveOperation(deviceId, app.db)
+	e = dro.Build()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 1, "expected len(l) == 1, got:", len(l))
+		e = MarkPendingOperationsStale(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		return nil
+	})
+
+	oc := OperationCleaner{
+		db:       app.db,
+		executor: app.executor,
+		sel:      CleanAll,
+	}
+	e = oc.Clean()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	// the non cleanable device remove operation remains
+	app.db.View(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 1, "expected len(l) == 1, got:", len(l))
+		return nil
+	})
+}
