da90bf8481d4951849e4c0f611bd50c2dba3ff33
John Mulligan <jmulligan@redhat.com>
2018-11-15 16:52:26 -0500

    apps: add a test for clean up of expand operation
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/operations_cleanup_test.go
+++ apps/glusterfs/operations_cleanup_test.go
@@ -224,3 +224,73 @@ func TestOperationsCleanupSkipNonLoadable(t *testing.T) {
 		return nil
 	})
 }
+
+func TestOperationsCleanupVolumeExpand(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	req := &api.VolumeCreateRequest{}
+	req.Size = 1024
+	req.Durability.Type = api.DurabilityReplicate
+	req.Durability.Replicate.Replica = 3
+
+	// create a volume we can delete later
+	vol := NewVolumeEntryFromRequest(req)
+	vc := NewVolumeCreateOperation(vol, app.db)
+	e := RunOperation(vc, app.executor)
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 0, "expected len(l) == 0, got:", len(l))
+		return nil
+	})
+
+	ve := NewVolumeExpandOperation(vol, app.db, 50)
+	e = ve.Build()
+	tests.Assert(t, e == nil, "expected e == nil, got:", e)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		po, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(po) == 1, "expected len(po) == 1, got:", len(po))
+		e = MarkPendingOperationsStale(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		return nil
+	})
+
+	oc := OperationCleaner{
+		db:       app.db,
+		executor: app.executor,
+		sel:      CleanAll,
+	}
+	e = oc.Clean()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.View(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 0, "expected len(l) == 0, got:", len(l))
+		vl, e := VolumeList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(vl) == 1, "expected len(vl) == 1, got:", len(l))
+		vol, e := NewVolumeEntryFromId(tx, vl[0])
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, vol.Info.Size == 1024,
+			"expected vol.Info.Size == 1024, got:", vol.Info.Size)
+		return nil
+	})
+}
