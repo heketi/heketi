5bb5f7465138b51c15ba6e6352a93deff144f7ec
John Mulligan <jmulligan@redhat.com>
2018-11-16 15:28:18 -0500

    apps: add tests for block volume cleanup
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/operations_cleanup_test.go
+++ apps/glusterfs/operations_cleanup_test.go
@@ -294,3 +294,105 @@ func TestOperationsCleanupVolumeExpand(t *testing.T) {
 		return nil
 	})
 }
+
+func TestOperationsCleanupBlockVolumeCreate(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	req := &api.BlockVolumeCreateRequest{}
+	req.Size = 1024
+
+	vol := NewBlockVolumeEntryFromRequest(req)
+	vc := NewBlockVolumeCreateOperation(vol, app.db)
+	e := vc.Build()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 1, "expected len(l) == 1, got:", len(l))
+		e = MarkPendingOperationsStale(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		return nil
+	})
+
+	oc := OperationCleaner{
+		db:       app.db,
+		executor: app.executor,
+		sel:      CleanAll,
+	}
+	e = oc.Clean()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.View(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 0, "expected len(l) == 0, got:", len(l))
+		return nil
+	})
+}
+
+func TestOperationsCleanupBlockVolumeDelete(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	req := &api.BlockVolumeCreateRequest{}
+	req.Size = 1024
+
+	vol := NewBlockVolumeEntryFromRequest(req)
+	vc := NewBlockVolumeCreateOperation(vol, app.db)
+	e := RunOperation(vc, app.executor)
+	tests.Assert(t, e == nil, "expected e == nil, got:", e)
+
+	vdel := NewBlockVolumeDeleteOperation(vol, app.db)
+	e = vdel.Build()
+	tests.Assert(t, e == nil, "expected e == nil, got:", e)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 1, "expected len(l) == 1, got:", len(l))
+		e = MarkPendingOperationsStale(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		return nil
+	})
+
+	oc := OperationCleaner{
+		db:       app.db,
+		executor: app.executor,
+		sel:      CleanAll,
+	}
+	e = oc.Clean()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.View(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 0, "expected len(l) == 0, got:", len(l))
+		return nil
+	})
+}
