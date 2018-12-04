62ed59cfcdc153144ae91e6b8eb90ea2bdeaf680
John Mulligan <jmulligan@redhat.com>
2018-12-04 14:08:44 -0500

    apps: add a test for background cleanup deferred via throttle
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/operations_cleanup_test.go
+++ apps/glusterfs/operations_cleanup_test.go
@@ -865,3 +865,91 @@ func TestBackgroundOperationCleaner(t *testing.T) {
 		return nil
 	})
 }
+
+func TestBackgroundOperationCleanerWithTracking(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	req := &api.VolumeCreateRequest{}
+	req.Size = 1024
+	req.Name = "vol1"
+	req.Durability.Type = api.DurabilityReplicate
+	req.Durability.Replicate.Replica = 3
+
+	vol := NewVolumeEntryFromRequest(req)
+	vc := NewVolumeCreateOperation(vol, app.db)
+	e := vc.Build()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 1, "expected len(l) == 1, got:", len(l))
+		e = MarkPendingOperationsStale(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		return nil
+	})
+
+	ot := newOpTracker(8)
+	boc := &backgroundOperationCleaner{
+		cleaner: OperationCleaner{
+			db:        app.db,
+			executor:  app.executor,
+			sel:       CleanAll,
+			optracker: ot,
+			opClass:   TrackClean,
+		},
+		StartInterval: 5 * time.Millisecond,
+		CheckInterval: 50 * time.Millisecond,
+	}
+	ot.Add("FAKEFAKE", TrackClean)
+
+	boc.Start()
+	defer boc.Stop()
+
+	time.Sleep(10 * time.Millisecond)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 1, "expected len(l) == 1, got:", len(l))
+		return nil
+	})
+
+	req.Name = "vol2"
+	vol2 := NewVolumeEntryFromRequest(req)
+	vc = NewVolumeCreateOperation(vol2, app.db)
+	e = vc.Build()
+	tests.Assert(t, e == nil, "expected e == nil, got", e)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 2, "expected len(l) == 2, got:", len(l))
+		e = MarkPendingOperationsStale(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		return nil
+	})
+
+	ot.Remove("FAKEFAKE")
+	time.Sleep(100 * time.Millisecond)
+
+	app.db.Update(func(tx *bolt.Tx) error {
+		l, e := PendingOperationList(tx)
+		tests.Assert(t, e == nil, "expected e == nil, got", e)
+		tests.Assert(t, len(l) == 0, "expected len(l) == 0, got:", len(l))
+		return nil
+	})
+}
