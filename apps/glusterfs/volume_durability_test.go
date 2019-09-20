29ca5d7fdd53699d2ad5864de837ebe4eb346388
olivergregorius <developer@verioweb.de>
2019-09-20 21:53:11 +0200

    Revert previous commit for triggering new build

--- apps/glusterfs/volume_durability_test.go
+++ apps/glusterfs/volume_durability_test.go
@@ -4,7 +4,7 @@
 // This file is licensed to you under your choice of the GNU Lesser
 // General Public License, version 3 or any later version (LGPLv3 or
 // later), or the GNU General Public License, version 2 (GPLv2), in all
-// cases as published by the Free Software Foundation. 
+// cases as published by the Free Software Foundation.
 //
 
 package glusterfs
