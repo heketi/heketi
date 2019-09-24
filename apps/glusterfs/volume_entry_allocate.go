361f23a66259e4a6dfb04c6d59ab6dbeb3aaf67c
John Mulligan <jmulligan@redhat.com>
2019-09-24 11:12:11 -0400

    apps: support tag matching on devices inheriting from nodes
    Make tag matching behave similar to arbiter tagging in that tags applied
    to nodes are inherited by the devices except when the devices have
    their own explicit tags.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/volume_entry_allocate.go
+++ apps/glusterfs/volume_entry_allocate.go
@@ -262,7 +262,7 @@ func (v *VolumeEntry) prepForBrickReplacement(db wdb.DB,
 	return
 }
 
-func (v *VolumeEntry) generateDeviceFilter(db wdb.RODB) (DeviceFilter, error) {
+func (v *VolumeEntry) generateDeviceFilter(db wdb.RODB, dsrc DeviceSource) (DeviceFilter, error) {
 
 	var filter DeviceFilter = nil
 	zoneChecking := v.GetZoneCheckingStrategy()
@@ -291,7 +291,7 @@ func (v *VolumeEntry) generateDeviceFilter(db wdb.RODB) (DeviceFilter, error) {
 			"Invalid tag matching rule: %v", err)
 	} else if tagMatchingRule != nil {
 		logger.Debug("Configuring a tag matching device filter")
-		filter = appendDeviceFilter(filter, tagMatchingRule.Filter)
+		filter = appendDeviceFilter(filter, tagMatchingRule.GetFilter(dsrc))
 	}
 
 	return filter, nil
@@ -311,9 +311,10 @@ func (v *VolumeEntry) allocBrickReplacement(db wdb.DB,
 			return oldDeviceEntry.Info.Id != d.Info.Id
 		}
 
+		dsrc := NewClusterDeviceSource(tx, v.Info.Cluster)
 		var err error
 		txdb := wdb.WrapTx(tx)
-		defaultFilter, err := v.generateDeviceFilter(txdb)
+		defaultFilter, err := v.generateDeviceFilter(txdb, dsrc)
 		if err != nil {
 			return err
 		}
@@ -328,7 +329,7 @@ func (v *VolumeEntry) allocBrickReplacement(db wdb.DB,
 
 		placer := PlacerForVolume(v)
 		r, err = placer.Replace(
-			NewClusterDeviceSource(tx, v.Info.Cluster),
+			dsrc,
 			NewVolumePlacementOpts(v, oldBrickEntry.Info.Size, bs.SetSize),
 			deviceFilter, bs, index)
 		if err == ErrNoSpace {
@@ -518,7 +519,7 @@ func (v *VolumeEntry) allocBricks(
 		placer := PlacerForVolume(v)
 
 		txdb := wdb.WrapTx(tx)
-		deviceFilter, err := v.generateDeviceFilter(txdb)
+		deviceFilter, err := v.generateDeviceFilter(txdb, dsrc)
 		if err != nil {
 			return err
 		}
