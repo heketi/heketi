b6c0232eb455b6971dfc4a278912e289cd145350
John Mulligan <jmulligan@redhat.com>
2019-09-24 11:44:54 -0400

    apps: add tests for TagMatchingRule GetFilter function
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/tag_matching_rule_test.go
+++ apps/glusterfs/tag_matching_rule_test.go
@@ -72,3 +72,82 @@ func TestTagMatchingRuleTest(t *testing.T) {
 		tests.Assert(t, tmr.Test("blob"))
 	})
 }
+
+func TestTagMatchingRuleFilter(t *testing.T) {
+	dsrc := NewTestDeviceSource()
+	var (
+		n         *NodeEntry
+		d         *DeviceEntry
+		deviceAdd func(string, string) *DeviceEntry
+	)
+	n, deviceAdd = tmrNodeAdd(dsrc, "foo")
+	n.Info.Tags["charles"] = "brown"
+	d = deviceAdd("foo000d1", "/dev/d1")
+	d.Info.Tags["tier"] = "one"
+
+	t.Run("on dev", func(t *testing.T) {
+		tmr := &TagMatchingRule{"tier", "=", "one"}
+		f := tmr.GetFilter(dsrc)
+		res := f(nil, d)
+		tests.Assert(t, res)
+	})
+	t.Run("on node", func(t *testing.T) {
+		tmr := &TagMatchingRule{"charles", "=", "brown"}
+		f := tmr.GetFilter(dsrc)
+		res := f(nil, d)
+		tests.Assert(t, res)
+	})
+	t.Run("no match on node or dev", func(t *testing.T) {
+		tmr := &TagMatchingRule{"charles", "!=", "xavier"}
+		f := tmr.GetFilter(dsrc)
+		res := f(nil, d)
+		tests.Assert(t, res)
+	})
+	t.Run("not on node or dev", func(t *testing.T) {
+		tmr := &TagMatchingRule{"charles", "=", "darwin"}
+		f := tmr.GetFilter(dsrc)
+		res := f(nil, d)
+		tests.Assert(t, !res)
+	})
+
+	// test invalid lookup. for completeness & coverage
+	dx := NewDeviceEntry()
+	dx.Info.Id = "bob"
+	dx.Info.Name = "bad"
+	dx.NodeId = "xxx"
+	dsrc.AddDevice(dx)
+
+	t.Run("with bad lookup", func(t *testing.T) {
+		tmr := &TagMatchingRule{"yeah", "=", "fangoriously"}
+		f := tmr.GetFilter(dsrc)
+		res := f(nil, dx)
+		tests.Assert(t, !res)
+	})
+}
+
+func tmrNodeAdd(tds *TestDeviceSource, nodeId string) (*NodeEntry, func(string, string) *DeviceEntry) {
+
+	n := NewNodeEntry()
+	n.Info.Id = nodeId
+	n.Info.Zone = 1
+	n.Info.Hostnames.Manage = []string{"mng-" + nodeId}
+	n.Info.Hostnames.Storage = []string{"stor-" + nodeId}
+	n.Info.ClusterId = "0000000000c"
+	n.Info.Tags = map[string]string{}
+	tds.AddNode(n)
+
+	f := func(deviceId, dname string) *DeviceEntry {
+		d := NewDeviceEntry()
+		d.Info.Id = deviceId
+		d.Info.Name = dname
+		d.Info.Storage.Total = 100
+		d.Info.Storage.Free = 100
+		d.NodeId = nodeId
+		d.Info.Tags = map[string]string{}
+
+		n.Devices = append(n.Devices, d.Info.Id)
+		tds.AddDevice(d)
+		return d
+	}
+	return n, f
+}
