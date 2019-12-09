5a572f2a56a3257230caa1c71f0bfb3077c26b76
Raghavendra Talur <rtalur@redhat.com>
2019-12-09 17:36:08 -0500

    tests: use same variable name as in run.sh
    Some of the variables are GLOBAL and are used further in other scripts.
    
    Signed-off-by: Raghavendra Talur <rtalur@redhat.com>

--- tests/functional/TestSmokeTest/teardown.sh
+++ tests/functional/TestSmokeTest/teardown.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CURRENT_DIR="$(pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd)"
 export HEKETI_SERVER_BUILD_DIR=../../..
-FUNCTIONAL_DIR="${CURRENT_DIR}/.."
+FUNCTIONAL_DIR="${SCRIPT_DIR}/.."
 export HEKETI_SERVER="${FUNCTIONAL_DIR}/heketi-server"
 
 source "${FUNCTIONAL_DIR}/lib.sh"
