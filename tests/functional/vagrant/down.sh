8a125753ad051f19d1025fde916358751e5cde4d
John Mulligan <jmulligan@redhat.com>
2020-07-15 17:11:06 -0400

    xxx
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/functional/vagrant/down.sh
+++ tests/functional/vagrant/down.sh
@@ -8,10 +8,10 @@ cd "${SCRIPT_DIR}"
 set -x
 for hn in storage0 storage1 storage2 storage3 ; do
     echo '+++' $hn
-    vagrant ssh $hn -- rpm -qa | grep gluster
-    vagrant ssh $hn -- systemctl status gluster-blockd
-    vagrant ssh $hn -- systemctl status gluster-blockd
-    vagrant ssh $hn -- sudo grep . /var/log/gluster-block/gluster-block-cli.log /var/log/gluster-block/gluster-block-configshell.log /var/log/gluster-block/gluster-block-gfapi.log /var/log/gluster-block/gluster-blockd.log
+    vagrant ssh $hn -- rpm -qa | grep gluster || true
+    vagrant ssh $hn -- systemctl status gluster-blockd || true
+    vagrant ssh $hn -- systemctl status gluster-blockd || true
+    vagrant ssh $hn -- sudo grep . /var/log/gluster-block/gluster-block-cli.log /var/log/gluster-block/gluster-block-configshell.log /var/log/gluster-block/gluster-block-gfapi.log /var/log/gluster-block/gluster-blockd.log || true
 done
 
 vagrant destroy -f
