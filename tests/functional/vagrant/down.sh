c60a505ed007b9ed67ae6c0a2b24d3882b4c0291
John Mulligan <jmulligan@redhat.com>
2020-07-15 15:59:12 -0400

    xxx: debug attempt
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/functional/vagrant/down.sh
+++ tests/functional/vagrant/down.sh
@@ -5,6 +5,15 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd)"
 cd "${SCRIPT_DIR}"
 
+set -x
+for hn in storage0 storage1 storage2 storage3 ; do
+    echo '+++' $hn
+    vagrant ssh $hn -- rpm -qa | grep gluster
+    vagrant ssh $hn -- systemctl status gluster-blockd
+    vagrant ssh $hn -- systemctl status gluster-blockd
+    vagrant ssh $hn -- sudo grep . /var/log/gluster-block/gluster-block-cli.log /var/log/gluster-block/gluster-block-configshell.log /var/log/gluster-block/gluster-block-gfapi.log /var/log/gluster-block/gluster-blockd.log
+done
+
 vagrant destroy -f
 for i in $(virsh vol-list default | grep '\.disk' | awk '{print $1}'); do
     virsh vol-delete --pool default "${i}"
